16032221fa54565c3488fd142342919665eb3d90
nabil alaa <nabilalaa06@gmail.com>
2023-10-04 12:41:06 +0300

    challenge_me

--- challenge_me/urls.py
+++ challenge_me/urls.py
@@ -4,6 +4,9 @@ from .views import *
 urlpatterns = [
     path('', index, name="home"),
 
+    # authentication
+    path('sign_in', sign_in, name='sign_in'),
+    path('login', login_view, name='login'),
     path('logout', logout_view, name='logout'),
 
     path('tournaments/<str:slug>', tournaments, name="tournaments"),
--- challenge_me/views.py
+++ challenge_me/views.py
@@ -6,6 +6,63 @@ from django.contrib import messages
 
 
 def index(request):
+    # name = request.POST.get("username")
+    # email = request.POST.get("email")
+    # password = request.POST.get("password")
+    # confirm_password = request.POST.get("confirm_password")
+    # if request.method == "POST":
+    #     if not User.objects.filter(username=name).exists():
+    #         if not User.objects.filter(email=email).exists():
+    #             if password == confirm_password:
+    #                 if len(password) >= 8 and len(confirm_password) >= 8:
+    #                     User.objects.create_user(
+    #                         username=name,
+    #                         email=email,
+    #                         password=password
+    #                     )
+    #                     messages.success(
+    #                         request, "register has been successfully")
+    #                     return redirect("/#subscription")
+
+    #                 else:
+    #                     messages.error(
+    #                         request, "password must be Greater than 8 letters ")
+    #                     print("password is exist")
+    #                     return redirect("/#subscription")
+    #             else:
+    #                 messages.error(
+    #                     request, "password is not the same confirm password")
+    #                 print("password is exist")
+    #                 return redirect("/#subscription")
+
+    #         else:
+    #             messages.error(
+    #                 request, "email is exist")
+    #             print("email is exist")
+    #             return redirect("/#subscription")
+
+    #     elif authenticate(request, username=name, password=password):
+    #         print(authenticate(request, username=name, password=password))
+
+    #         login(request, authenticate(
+    #             request, username=name, password=password))
+    #         return redirect(index)
+    #     else:
+
+    #         messages.error(
+    #             request, "username is exist")
+    #         print("username is exist")
+    #         return redirect("/#subscription")
+
+    context = {
+        "add_game": AddGame.objects.all(),
+        "add_about": About.objects.all(),
+    }
+
+    return render(request, "index.html", context)
+
+
+def sign_in(request):
     name = request.POST.get("username")
     email = request.POST.get("email")
     password = request.POST.get("password")
@@ -40,13 +97,6 @@ def index(request):
                     request, "email is exist")
                 print("email is exist")
                 return redirect("/#subscription")
-
-        elif authenticate(request, username=name, password=password):
-            print(authenticate(request, username=name, password=password))
-
-            login(request, authenticate(
-                request, username=name, password=password))
-            return redirect(index)
         else:
 
             messages.error(
@@ -54,12 +104,24 @@ def index(request):
             print("username is exist")
             return redirect("/#subscription")
 
-    context = {
-        "add_game": AddGame.objects.all(),
-        "add_about": About.objects.all(),
-    }
+    return redirect("/")
 
-    return render(request, "index.html", context)
+
+def login_view(request):
+    name = request.POST.get("username")
+    password = request.POST.get("password")
+
+    if authenticate(request, username=name, password=password):
+        print(authenticate(request, username=name, password=password))
+
+        login(request, authenticate(
+            request, username=name, password=password))
+    else:
+        messages.error(
+            request, "username or password is wrong")
+        return redirect("/#subscription")
+
+    return redirect(index)
 
 
 def logout_view(request):
