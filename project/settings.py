b4630577f23f59a60fe13a2b791b1592fe9286e6
nabil alaa <nabilalaa06@gmail.com>
2025-06-27 22:17:00 +0300

    challenge_me

--- project/settings.py
+++ project/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'corsheaders.middleware.CorsMiddleware',
-
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
